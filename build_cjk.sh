930100dde7ab29b8673f4ea03f7109a7f8f7786e
daniel.feng <daniel.feng@fanruan.com>
2023-02-23 19:59:49 +0800

    add cjk support

---
+++ build_cjk.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+version="0.76.1"
+docker build --pull --rm -f "Dockerfile.base" -t sldaniel/outline-base:latest "." && \
+docker build --rm -f "Dockerfile" -t sldaniel/outline:latest -t sldaniel/outline:$version "." && \
+docker push sldaniel/outline:$version && \
+docker push sldaniel/outline:latest
+docker image rm sldaniel/outline:$version sldaniel/outline:latest sldaniel/outline-base:latest
