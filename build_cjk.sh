6ddef5c1a0f947d25cb07076212d674cf9fa9e31
danfate <sl474118737@gmail.com>
2023-08-22 22:58:01 +0800

    v0.71.0

--- build_cjk.sh
+++ build_cjk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-version="0.68.1"
+version="0.71.0"
 docker build --pull --rm -f "Dockerfile.base" -t sldaniel/outline-base:latest "." && \
 docker build --rm -f "Dockerfile" -t sldaniel/outline:$version "." && \
 docker push sldaniel/outline:$version && \
