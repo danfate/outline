8f74b912c21c66d91155ef520f6d8ef956016a39
danfate <sl474118737@gmail.com>
2024-06-30 22:14:53 +0800

    add cjk support

--- build_cjk.sh
+++ build_cjk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-version="0.76.1"
+version="0.77.0"
 docker build --pull --rm -f "Dockerfile.base" -t sldaniel/outline-base:latest "." && \
 docker build --rm -f "Dockerfile" -t sldaniel/outline:latest -t sldaniel/outline:$version "." && \
 docker push sldaniel/outline:$version && \
