74df09b1ff131bbb1b2eb33211f2a1825d178537
danfate <sl474118737@gmail.com>
2024-06-30 23:25:49 +0800

    test2

--- build_cjk.sh
+++ build_cjk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-version="0.77.0-1-test"
+version="0.77.0-0-test"
 docker build --pull --rm -f "Dockerfile.base" -t sldaniel/outline-base:latest "." && \
 docker build --rm -f "Dockerfile" -t sldaniel/outline:latest -t sldaniel/outline:$version "." && \
 docker push sldaniel/outline:$version && \
